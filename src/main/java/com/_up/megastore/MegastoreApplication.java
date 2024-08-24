bff14f6d1a449b95504ff0d4f38cd02a27a54522
EzeSosa <ezequielsosa423@gmail.com>
2024-08-24 16:53:17 -0300

    init project

---
+++ src/main/java/com/_up/megastore/MegastoreApplication.java
@@ -0,0 +1,11 @@
+package com._up.megastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegastoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MegastoreApplication.class, args);
+	}
+}
