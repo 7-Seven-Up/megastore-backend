e4fdcc10ecbf906398553ab276b20e8e294f3d82
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-08-31 14:33:28 -0300

    get product

--- src/main/java/com/_up/megastore/controllers/interfaces/IProductController.java
+++ src/main/java/com/_up/megastore/controllers/interfaces/IProductController.java
@@ -1,22 +1,31 @@
 package com._up.megastore.controllers.interfaces;
 
-import com._up.megastore.controllers.requests.CreateProductRequest;
-import com._up.megastore.controllers.requests.UpdateProductRequest;
-import com._up.megastore.controllers.responses.ProductResponse;
-import jakarta.validation.Valid;
+import java.util.UUID;
+
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.lang.Nullable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.UUID;
+import com._up.megastore.controllers.requests.CreateProductRequest;
+import com._up.megastore.controllers.requests.UpdateProductRequest;
+import com._up.megastore.controllers.responses.ProductResponse;
+
+import jakarta.validation.Valid;
 
 @RequestMapping("/api/v1/products")
 public interface IProductController {
 
-    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.CREATED)
     ProductResponse saveProduct(
             @RequestPart @Valid CreateProductRequest createProductRequest,
@@ -27,7 +36,7 @@ public interface IProductController {
     @ResponseStatus(HttpStatus.OK)
     ProductResponse findProductById(@PathVariable UUID id);
 
-    @PatchMapping(value = "/{productId}", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    @PatchMapping(value = "/{productId}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.OK)
     ProductResponse updateProduct(
             @PathVariable UUID productId,
