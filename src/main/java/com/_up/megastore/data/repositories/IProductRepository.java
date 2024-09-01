672d68710db1481fda899aaa190666aa1270e550
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-09-01 16:22:26 -0300

    remove JPASpecifications

--- src/main/java/com/_up/megastore/data/repositories/IProductRepository.java
+++ src/main/java/com/_up/megastore/data/repositories/IProductRepository.java
@@ -4,12 +4,9 @@ import com._up.megastore.data.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import java.util.*;
 
-import java.util.Optional;
-import java.util.UUID;
-
-public interface IProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
+public interface IProductRepository extends JpaRepository<Product, UUID> {
     Page<Product> findProductsByDeletedIsFalseAndNameLikeIgnoreCase(String filter, Pageable pageable);
     Optional<Product> findProductByProductIdAndDeletedIsFalse(UUID productId);
 }
