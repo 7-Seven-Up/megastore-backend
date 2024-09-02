ef87cf6f69fc5c1a1aec8e9c412efe31bcf6080c
Matias <mmatidelga2@gmail.com>
2024-09-01 21:21:30 -0300

    feat: extend user repository with email and username checks

--- src/main/java/com/_up/megastore/data/repositories/IUserRepository.java
+++ src/main/java/com/_up/megastore/data/repositories/IUserRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface IUserRepository extends JpaRepository<User, UUID> {}
+public interface IUserRepository extends JpaRepository<User, UUID> {
+  boolean existsByEmail(String email);
+
+  boolean existsByUsername(String username);
+}
