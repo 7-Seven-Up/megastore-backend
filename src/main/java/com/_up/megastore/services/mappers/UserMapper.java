78a0858ee4469444e94b2c3954bf0e15cd7e338b
Matias <mmatidelga2@gmail.com>
2024-09-04 15:27:59 -0300

    ref: only map to entity and do not convert the phone

--- src/main/java/com/_up/megastore/services/mappers/UserMapper.java
+++ src/main/java/com/_up/megastore/services/mappers/UserMapper.java
@@ -1,31 +1,16 @@
 package com._up.megastore.services.mappers;
 
-import com._up.megastore.controllers.requests.CreateUserRequest;
-import com._up.megastore.controllers.responses.UserResponse;
+import com._up.megastore.controllers.requests.SignUpRequest;
 import com._up.megastore.data.model.User;
-import com._up.megastore.data.utilities.PhoneNumberConversion;
 
 public class UserMapper {
 
-  public static UserResponse toUserResponse(User user) {
-    return new UserResponse(
-        user.getUserId(),
-        user.getUsername(),
-        user.getFullName(),
-        user.getEmail(),
-        user.getPhoneNumber(),
-        user.getRole()
-    );
-  }
-
-  public static User toUser(CreateUserRequest createUserRequest) {
+  public static User toUser(SignUpRequest createUserRequest, String convertedPhoneNumber) {
     return User.builder()
         .email(createUserRequest.email())
         .fullName(createUserRequest.fullName())
         .password(createUserRequest.password())
-        .phoneNumber(
-            PhoneNumberConversion.convertPhoneNumber(createUserRequest.phoneNumber())
-        )
+        .phoneNumber(convertedPhoneNumber)
         .username(createUserRequest.username())
         .build();
   }
