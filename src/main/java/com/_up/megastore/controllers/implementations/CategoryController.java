5d7960fb418c80656f78516215418f0cc47a17a3
martinjcrosetto <85657891+martinjcrosetto@users.noreply.github.com>
2024-09-05 17:19:18 -0300

    feat: update category (#26)
    * feat : UpdateCategory
    
    * feat: added a functionality for update superCategory in CategoryService
    
    * ref : changed parameters ifSuperCategoryExistUpdateCategorySuperCategory method and added a compare condition to know if superCategoryId isnt the same.

--- src/main/java/com/_up/megastore/controllers/implementations/CategoryController.java
+++ src/main/java/com/_up/megastore/controllers/implementations/CategoryController.java
@@ -2,9 +2,12 @@ package com._up.megastore.controllers.implementations;
 
 import com._up.megastore.controllers.interfaces.ICategoryController;
 import com._up.megastore.controllers.requests.CreateCategoryRequest;
+import com._up.megastore.controllers.requests.UpdateCategoryRequest;
 import com._up.megastore.controllers.responses.CategoryResponse;
 import com._up.megastore.services.interfaces.ICategoryService;
+import org.hibernate.sql.Update;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
 
 @RestController
 public class CategoryController implements ICategoryController {
@@ -20,4 +23,8 @@ public class CategoryController implements ICategoryController {
         return categoryService.saveCategory(createCategoryRequest);
     }
 
+    @Override
+    public CategoryResponse updateCategory(UUID categoryId, UpdateCategoryRequest updateCategoryRequest){
+        return categoryService.updateCategory(categoryId,updateCategoryRequest);
+    }
 }
