a51aa1063b013f960e1d5001b3b46d66dfaab756
Matias <mmatidelga2@gmail.com>
2024-09-08 16:24:18 -0300

    ref: remove enable async

--- src/main/java/com/_up/megastore/MegastoreApplication.java
+++ src/main/java/com/_up/megastore/MegastoreApplication.java
@@ -2,10 +2,8 @@ package com._up.megastore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class MegastoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MegastoreApplication.class, args);
