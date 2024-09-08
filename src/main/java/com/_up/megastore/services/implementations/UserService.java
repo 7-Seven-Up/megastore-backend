7e829fee168d5e7ba34c8e1ed05ec83bda67bb50
Matias <mmatidelga2@gmail.com>
2024-09-08 16:22:02 -0300

    feat: send activation email

--- src/main/java/com/_up/megastore/services/implementations/UserService.java
+++ src/main/java/com/_up/megastore/services/implementations/UserService.java
@@ -3,18 +3,27 @@ package com._up.megastore.services.implementations;
 import com._up.megastore.controllers.requests.SignUpRequest;
 import com._up.megastore.data.model.User;
 import com._up.megastore.data.repositories.IUserRepository;
+import com._up.megastore.services.interfaces.IEmailService;
 import com._up.megastore.services.interfaces.IUserService;
 import com._up.megastore.services.mappers.UserMapper;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements IUserService {
 
+  private final IEmailService emailService;
   private final IUserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
 
-  public UserService(IUserRepository userRepository, PasswordEncoder passwordEncoder) {
+  @Value("${frontend.url}")
+  private String frontendURL;
+
+  public UserService(IUserRepository userRepository, PasswordEncoder passwordEncoder,
+      IEmailService emailService) {
+    this.emailService = emailService;
     this.passwordEncoder = passwordEncoder;
     this.userRepository = userRepository;
   }
@@ -24,7 +33,15 @@ public class UserService implements IUserService {
     ifEmailAlreadyExistsThrowException(createUserRequest.email());
     ifUsernameAlreadyExistsThrowException(createUserRequest.username());
     User newUser = createUser(createUserRequest);
+    sendActivationEmail(newUser);
     userRepository.save(newUser);
+  }
+
+  @Override
+  public void activateUser(UUID userId, UUID activationToken) {
+    User user = findUserToActivateOrThrowException(userId, activationToken);
+    user.setActivated(true);
+    userRepository.save(user);
   }
 
   User createUser(SignUpRequest createUserRequest) {
@@ -32,6 +49,29 @@ public class UserService implements IUserService {
     String passwordEncoded = passwordEncoder.encode(createUserRequest.password());
     user.setPassword(passwordEncoded);
     return user;
+  }
+
+  private void sendActivationEmail(User user) {
+    String activationUrl = frontendURL + "/auth/activate?userId="
+        + user.getUserId() + "&activationToken=" + user.getActivationToken();
+
+    String emailBody = "<table style='width:100%; height:100%;'>"
+        + "<tr>"
+        + "<td style='width:100%; height:100%; text-align:center; vertical-align:middle;'>"
+        + "<div style='display:inline-block;'>"
+        + "<h1>Welcome to Megastore</h1>"
+        + "<h3>Hey " + user.getFullName() + "!</h3>"
+        + "<h4>Thanks for joining us!</h4>"
+        + "<p>Click the link below to activate your account</p>"
+        + "<a href=\""
+        + activationUrl
+        + "\">Activate account</a>"
+        + "</div>"
+        + "</td>"
+        + "</tr>"
+        + "</table>";
+
+    emailService.sendEmail(user.getEmail(), "Account activation", emailBody);
   }
 
   private void ifEmailAlreadyExistsThrowException(String email) {
@@ -45,4 +85,13 @@ public class UserService implements IUserService {
       throw new IllegalArgumentException("Username already exists");
     }
   }
+
+  private User findUserToActivateOrThrowException(UUID userId,
+      UUID activationToken) {
+    return userRepository.findByUserIdAndActivationTokenAndActivatedIsFalse(userId,
+            activationToken)
+        .orElseThrow(
+            () -> new IllegalArgumentException("Token is invalid or user is already activated"));
+  }
+
 }
--- src/main/java/com/_up/megastore/services/interfaces/IUserService.java
+++ src/main/java/com/_up/megastore/services/interfaces/IUserService.java
@@ -1,8 +1,11 @@
 package com._up.megastore.services.interfaces;
 
 import com._up.megastore.controllers.requests.SignUpRequest;
+import java.util.UUID;
 
 public interface IUserService {
 
   void saveUser(SignUpRequest newUser);
+
+  void activateUser(UUID userId, UUID activationToken);
 }
