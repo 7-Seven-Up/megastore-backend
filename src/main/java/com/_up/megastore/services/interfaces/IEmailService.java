ec24bf11a0af162e8f372eaf53be05584f4080b7
Matias <mmatidelga2@gmail.com>
2024-09-08 16:17:08 -0300

    ref: remove async

--- src/main/java/com/_up/megastore/services/interfaces/IEmailService.java
+++ src/main/java/com/_up/megastore/services/interfaces/IEmailService.java
@@ -1,9 +1,6 @@
 package com._up.megastore.services.interfaces;
 
-import org.springframework.scheduling.annotation.Async;
-
 public interface IEmailService {
 
-  @Async
   void sendEmail(String to, String subject, String content);
 }
