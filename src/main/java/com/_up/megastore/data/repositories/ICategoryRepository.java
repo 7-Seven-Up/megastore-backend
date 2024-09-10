58ae7cbf541b3577b7a39458d6e40b7773637172
martin crosetto <martincrosetto312@gmail.com>
2024-09-09 23:53:34 -0300

    ref: import deletion in ICategoryRepository.

--- src/main/java/com/_up/megastore/data/repositories/ICategoryRepository.java
+++ src/main/java/com/_up/megastore/data/repositories/ICategoryRepository.java
@@ -2,8 +2,6 @@ package com._up.megastore.data.repositories;
 
 import com._up.megastore.data.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 import java.util.UUID;
 
 public interface ICategoryRepository extends JpaRepository<Category, UUID> {
