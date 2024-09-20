c1c20abca0f8e4a68c1ebe21eb3368e68657703f
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-09-20 15:45:37 -0300

    fix: update validations with frontend

--- src/main/java/com/_up/megastore/controllers/requests/RecoverPasswordRequest.java
+++ src/main/java/com/_up/megastore/controllers/requests/RecoverPasswordRequest.java
@@ -2,15 +2,15 @@ package com._up.megastore.controllers.requests;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
 public record RecoverPasswordRequest(
         @NotBlank(message = "Password must not be null")
+        @Length(min = 6, max = 20)
         @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!¡¿?$%&#@_-])[A-Za-z0-9!¡¿?$%&#@_-]+$", message = "The password are not strong.")
         String password,
-        @NotBlank(message = "Confirm Password must not be null")
-        @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!¡¿?$%&#@_-])[A-Za-z0-9!¡¿?$%&#@_-]+$", message = "The password are not strong.")
         String confirmPassword,
         @NotBlank(message = "RecoverPasswordToken must not be null")
         UUID recoverPasswordToken
