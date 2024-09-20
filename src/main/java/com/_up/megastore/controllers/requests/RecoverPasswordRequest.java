e527b4c26a08939cda36abbfea66993c732647ad
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-09-20 15:25:46 -0300

    fix: annotations for recover password request

--- src/main/java/com/_up/megastore/controllers/requests/RecoverPasswordRequest.java
+++ src/main/java/com/_up/megastore/controllers/requests/RecoverPasswordRequest.java
@@ -1,15 +1,15 @@
 package com._up.megastore.controllers.requests;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.UUID;
 
 public record RecoverPasswordRequest(
-        @NotNull(message = "Password must not be null")
+        @NotBlank(message = "Password must not be null")
         String password,
-        @NotNull(message = "Confirm Password must not be null")
+        @NotBlank(message = "Confirm Password must not be null")
         String confirmPassword,
-        @NotNull(message = "RecoverPasswordToken must not be null")
+        @NotBlank(message = "RecoverPasswordToken must not be null")
         UUID recoverPasswordToken
 )
 {}
