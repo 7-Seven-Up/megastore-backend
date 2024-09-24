1e1add498ba342e21e7f12440ffeba9ed76cbeca
EzeSosa <ezequielsosa423@gmail.com>
2024-09-24 02:34:26 -0300

    ref: async email sending

--- src/main/java/com/_up/megastore/services/interfaces/IEmailService.java
+++ src/main/java/com/_up/megastore/services/interfaces/IEmailService.java
@@ -1,6 +1,9 @@
 package com._up.megastore.services.interfaces;
 
+import org.springframework.scheduling.annotation.Async;
+
 public interface IEmailService {
 
+  @Async
   void sendEmail(String to, String subject, String content);
 }
