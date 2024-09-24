740d379119573536160b270be707abc74fb2d70b
Fedesan <fede14.sanchez@gmail.com>
2024-09-24 13:14:17 -0300

    ref: remove unused exception

--- src/main/java/com/_up/megastore/exception/ParsePhoneNumberException.java
+++
@@ -1,7 +0,0 @@
-package com._up.megastore.exception;
-
-public class ParsePhoneNumberException extends RuntimeException {
-    public ParsePhoneNumberException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
