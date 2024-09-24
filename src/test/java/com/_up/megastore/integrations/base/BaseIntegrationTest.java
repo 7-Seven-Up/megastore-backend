cee25092c1835fc34c7b80f3e49aa328eb310391
Federico Sanchez <71854831+Fedesan14@users.noreply.github.com>
2024-09-24 17:00:14 -0300

    test: add base integration test (#64)
    * test: add tests
    
    * ref: remove unused exception

---
+++ src/test/java/com/_up/megastore/integrations/base/BaseIntegrationTest.java
@@ -0,0 +1,31 @@
+package com._up.megastore.integrations.base;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class BaseIntegrationTest {
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected String toJson(Object request) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(request);
+    }
+
+    protected void assertContains(String response, String propertyName, String value) {
+        String valueToCheck = "\"" + propertyName + "\"" + ":" + "\"" + value + "\"";
+        Assertions.assertTrue(response.contains(valueToCheck));
+    }
+}
