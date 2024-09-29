4d5517cea352210154da53ea99b1700211d70942
EzeSosa <ezequielsosa423@gmail.com>
2024-09-29 02:20:32 -0300

    ref: send a custom new activation token email

--- src/main/java/com/_up/megastore/services/implementations/UserService.java
+++ src/main/java/com/_up/megastore/services/implementations/UserService.java
@@ -11,7 +11,7 @@ import com._up.megastore.services.interfaces.IEmailService;
 import com._up.megastore.services.interfaces.ITokenService;
 import com._up.megastore.services.interfaces.IUserService;
 import com._up.megastore.services.mappers.UserMapper;
-import org.springframework.beans.factory.annotation.Value;
+import com._up.megastore.utils.EmailBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -27,15 +27,15 @@ public class UserService implements IUserService {
   private final IUserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
   private final ITokenService tokenService;
-  @Value("${frontend.url}")
-  private String frontendURL;
+  private final EmailBuilder emailBuilder;
 
   public UserService(IUserRepository userRepository, PasswordEncoder passwordEncoder,
-      IEmailService emailService, ITokenService tokenService) {
+                     IEmailService emailService, ITokenService tokenService, EmailBuilder emailBuilder) {
     this.emailService = emailService;
     this.passwordEncoder = passwordEncoder;
     this.userRepository = userRepository;
     this.tokenService = tokenService;
+    this.emailBuilder = emailBuilder;
   }
 
   @Override
@@ -73,38 +73,12 @@ public class UserService implements IUserService {
   }
 
   private void sendActivationEmail(User user, UUID activationToken) {
-    String activationUrl = frontendURL + "/auth/activate?userId="
-        + user.getUserId() + "&activationToken=" + activationToken;
-
-    String emailBody = "<table style='width:100%; height:100%;'>"
-        + "<tr>"
-        + "<td style='width:100%; height:100%; text-align:center; vertical-align:middle;'>"
-        + "<div style='display:inline-block;'>"
-        + "<h1>Welcome to Megastore</h1>"
-        + "<h3>Hey " + user.getFullName() + "!</h3>"
-        + "<h4>Thanks for joining us!</h4>"
-        + "<p>Click the link below to activate your account</p>"
-        + "<a href=\""
-        + activationUrl
-        + "\">Activate account</a>"
-        + "</div>"
-        + "</td>"
-        + "</tr>"
-        + "</table>";
-
+    String emailBody = emailBuilder.buildActivationEmailBody(user, activationToken);
     emailService.sendEmail(user.getEmail(), "Account activation", emailBody);
   }
 
   private void sendWelcomeEmail(User user) {
-    String emailBody = "<table style='width:100%; height:100%;'>"
-        + "<tr><td style='width:100%; height:100%; text-align:center; vertical-align:middle;'>"
-        + "<div style='display:inline-block;'>"
-        + "<h1>Welcome to Megastore</h1>"
-        + "<h3>Hey " + user.getFullName() + "!</h3>"
-        + "<h4>Your account has been activated.</h4>"
-        + "<p>Enjoy shopping with us!</p>"
-        + "</div></td></tr></table>";
-
+    String emailBody = emailBuilder.buildWelcomeEmail(user);
     emailService.sendEmail(user.getEmail(), "Welcome to Megastore", emailBody);
   }
 
@@ -152,52 +126,7 @@ public class UserService implements IUserService {
 
     userRepository.save(user);
 
-    String recoverPasswordURL = frontendURL + "/auth/recover-password?token=" + user.getRecoverPasswordToken();
-
-    String emailContent = "<!DOCTYPE html>\n"
-            + "<html>\n"
-            + "<head>\n"
-            + "    <meta charset=\"UTF-8\">\n"
-            + "    <title>Password Recovery</title>\n"
-            + "    <style>\n"
-            + "        body { font-family: Arial, sans-serif; }\n"
-            + "        .container { width: 80%; margin: 0 auto; }\n"
-            + "        .header { background-color: #f4f4f4; padding: 20px; text-align: center; }\n"
-            + "        .content { padding: 20px; }\n"
-            + "        .footer { background-color: #f4f4f4; padding: 10px; text-align: center; font-size: 12px; }\n"
-            + "        .button {\n"
-            + "            display: inline-block;\n"
-            + "            padding: 10px 20px;\n"
-            + "            font-size: 16px;\n"
-            + "            color: #fff;\n"
-            + "            background-color: #1ae866;\n"
-            + "            text-decoration: none;\n"
-            + "            border-radius: 5px;\n"
-            + "            text-align: center;\n"
-            + "        }\n"
-            + "        .button:hover {\n"
-            + "            background-color: #15d67c;\n"
-            + "        }\n"
-            + "    </style>\n"
-            + "</head>\n"
-            + "<body>\n"
-            + "    <div class=\"container\">\n"
-            + "        <div class=\"header\">\n"
-            + "            <h1>Password Recovery</h1>\n"
-            + "        </div>\n"
-            + "        <div class=\"content\">\n"
-            + "            <p>Hello " + user.getUsername() + ",</p>\n"
-            + "            <p>We received a request to reset your password. Click the button below to create a new password:</p>\n"
-            + "            <p style=\"display: flex; flex: content; justify-content: center;\"><a href=\" " + recoverPasswordURL + "\" class=\"button\">Reset Password</a></p>\n"
-            + "            <p>If you did not request this change, please ignore this email.</p>\n"
-            + "            <p>Best regards,<br>The Support Team</p>\n"
-            + "        </div>\n"
-            + "        <div class=\"footer\">\n"
-            + "            <p>Megastore, Villa Maria, Cordoba, Argentina</p>\n"
-            + "        </div>\n"
-            + "    </div>\n"
-            + "</body>\n"
-            + "</html>";
+    String emailContent = emailBuilder.buildRecoverPasswordEmail(user);
     emailService.sendEmail(email, "Recover Password", emailContent);
   }
 
@@ -238,7 +167,12 @@ public class UserService implements IUserService {
     throwExceptionIfTokenIsNotExpired(sendNewActivationTokenRequest.activationToken());
     User user = tokenService.findUserByActivationToken(sendNewActivationTokenRequest.activationToken());
     Token token = tokenService.saveToken(user);
-    sendActivationEmail(user, token.getTokenId());
+    sendNewActivationEmail(user, token.getTokenId());
+  }
+
+  private void sendNewActivationEmail(User user, UUID activationToken) {
+    String emailBody = emailBuilder.buildNewActivationEmail(user, activationToken);
+    emailService.sendEmail(user.getEmail(), "New Activation Token", emailBody);
   }
 
   private void throwExceptionIfTokenIsNotExpired(UUID activationTokenId) {
---
+++ src/main/java/com/_up/megastore/utils/EmailBuilder.java
@@ -0,0 +1,116 @@
+package com._up.megastore.utils;
+
+import com._up.megastore.data.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class EmailBuilder {
+
+    @Value("${frontend.url}")
+    private String frontendURL;
+
+    public String buildActivationEmailBody(User user, UUID activationToken) {
+        String activationUrl = frontendURL + "/auth/activate?userId="
+                + user.getUserId() + "&activationToken=" + activationToken;
+
+        return "<table style='width:100%; height:100%;'>"
+                + "<tr>"
+                + "<td style='width:100%; height:100%; text-align:center; vertical-align:middle;'>"
+                + "<div style='display:inline-block;'>"
+                + "<h1>Welcome to Megastore</h1>"
+                + "<h3>Hey " + user.getFullName() + "!</h3>"
+                + "<h4>Thanks for joining us!</h4>"
+                + "<p>Click the link below to activate your account</p>"
+                + "<a href=\""
+                + activationUrl
+                + "\">Activate account</a>"
+                + "</div>"
+                + "</td>"
+                + "</tr>"
+                + "</table>";
+    }
+
+    public String buildRecoverPasswordEmail(User user) {
+        String recoverPasswordURL = frontendURL + "/auth/recover-password?token=" + user.getRecoverPasswordToken();
+
+        return "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "    <meta charset=\"UTF-8\">\n"
+                + "    <title>Password Recovery</title>\n"
+                + "    <style>\n"
+                + "        body { font-family: Arial, sans-serif; }\n"
+                + "        .container { width: 80%; margin: 0 auto; }\n"
+                + "        .header { background-color: #f4f4f4; padding: 20px; text-align: center; }\n"
+                + "        .content { padding: 20px; }\n"
+                + "        .footer { background-color: #f4f4f4; padding: 10px; text-align: center; font-size: 12px; }\n"
+                + "        .button {\n"
+                + "            display: inline-block;\n"
+                + "            padding: 10px 20px;\n"
+                + "            font-size: 16px;\n"
+                + "            color: #fff;\n"
+                + "            background-color: #1ae866;\n"
+                + "            text-decoration: none;\n"
+                + "            border-radius: 5px;\n"
+                + "            text-align: center;\n"
+                + "        }\n"
+                + "        .button:hover {\n"
+                + "            background-color: #15d67c;\n"
+                + "        }\n"
+                + "    </style>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "    <div class=\"container\">\n"
+                + "        <div class=\"header\">\n"
+                + "            <h1>Password Recovery</h1>\n"
+                + "        </div>\n"
+                + "        <div class=\"content\">\n"
+                + "            <p>Hello " + user.getUsername() + ",</p>\n"
+                + "            <p>We received a request to reset your password. Click the button below to create a new password:</p>\n"
+                + "            <p style=\"display: flex; flex: content; justify-content: center;\"><a href=\" " + recoverPasswordURL + "\" class=\"button\">Reset Password</a></p>\n"
+                + "            <p>If you did not request this change, please ignore this email.</p>\n"
+                + "            <p>Best regards,<br>The Support Team</p>\n"
+                + "        </div>\n"
+                + "        <div class=\"footer\">\n"
+                + "            <p>Megastore, Villa Maria, Cordoba, Argentina</p>\n"
+                + "        </div>\n"
+                + "    </div>\n"
+                + "</body>\n"
+                + "</html>";
+    }
+
+    public String buildWelcomeEmail(User user) {
+        return "<table style='width:100%; height:100%;'>"
+                + "<tr><td style='width:100%; height:100%; text-align:center; vertical-align:middle;'>"
+                + "<div style='display:inline-block;'>"
+                + "<h1>Welcome to Megastore</h1>"
+                + "<h3>Hey " + user.getFullName() + "!</h3>"
+                + "<h4>Your account has been activated.</h4>"
+                + "<p>Enjoy shopping with us!</p>"
+                + "</div></td></tr></table>";
+    }
+
+    public String buildNewActivationEmail(User user, UUID activationToken) {
+        String activationUrl = frontendURL + "/auth/activate?userId="
+                + user.getUserId() + "&activationToken=" + activationToken;
+
+        return "<table style='width:100%; height:100%;'>"
+                + "<tr>"
+                + "<td style='width:100%; height:100%; text-align:center; vertical-align:middle;'>"
+                + "<div style='display:inline-block;'>"
+                + "<h2>Activation Token Request</h2>"
+                + "<h3>Hello " + user.getFullName() + "!</h3>"
+                + "<h4>We've received a request for a new activation token.</h4>"
+                + "<p>If you requested this, click the link below to activate your account:</p>"
+                + "<a href=\"" + activationUrl + "\">Activate account</a>"
+                + "<p>If you did not request this, please ignore this email.</p>"
+                + "</div>"
+                + "</td>"
+                + "</tr>"
+                + "</table>";
+    }
+
+}
