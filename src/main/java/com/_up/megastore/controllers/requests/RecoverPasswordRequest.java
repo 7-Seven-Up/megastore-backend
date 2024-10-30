6b943c2301e36aeae5232360993759ada62115cc
EzeSosa <ezequielsosa423@gmail.com>
2024-10-30 18:13:19 -0300

    ref: keep signup validations

--- src/main/java/com/_up/megastore/controllers/requests/RecoverPasswordRequest.java
+++ src/main/java/com/_up/megastore/controllers/requests/RecoverPasswordRequest.java
@@ -1,17 +1,18 @@
 package com._up.megastore.controllers.requests;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 public record RecoverPasswordRequest(
-        @NotBlank(message = "Password must not be null")
-        @Length(min = 6, max = 20)
-        @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!¡¿?$%&#@_-])[A-Za-z0-9!¡¿?$%&#@_-]+$", message = "The password are not strong.")
+
+        @NotNull(message = "User password must not be null")
+        @Size(min = 6, max = 20, message = "User password must be between 6 and 20 characters")
         String password,
         String confirmPassword,
+
         @NotBlank(message = "RecoverPasswordToken must not be null")
         UUID recoverPasswordToken
 ) {}
