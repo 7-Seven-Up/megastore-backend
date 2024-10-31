a20f7f8c570fd3201f612b19416286241a42e82d
EzeSosa <ezequielsosa423@gmail.com>
2024-10-31 14:54:15 -0300

    ref: previous integration tests directory

--- src/test/java/com/_up/megastore/integrations/premade/ProductControllerTest.java
+++ src/test/java/com/_up/megastore/integrations/premade/ProductControllerTest.java
@@ -1,4 +1,4 @@
-package com._up.megastore.integrations.custom;
+package com._up.megastore.integrations.premade;
 
 import com._up.megastore.integrations.base.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
