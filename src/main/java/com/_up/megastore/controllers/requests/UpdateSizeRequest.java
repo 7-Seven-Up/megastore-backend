e44f8add805adfeea524aa989586a11c3cf36cce
EzeSosa <ezequielsosa423@gmail.com>
2024-11-03 12:25:14 -0300

    ref: size name validation

--- src/main/java/com/_up/megastore/controllers/requests/UpdateSizeRequest.java
+++ src/main/java/com/_up/megastore/controllers/requests/UpdateSizeRequest.java
@@ -1,11 +1,10 @@
 package com._up.megastore.controllers.requests;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UpdateSizeRequest (
-        @NotBlank(message = "Size name must not be null and blank")
+        @NotNull(message = "Size name must not be null")
         @Size(min = 1, max = 20, message = "Size name must be between 1 and 20 characters")
         String name,
 
