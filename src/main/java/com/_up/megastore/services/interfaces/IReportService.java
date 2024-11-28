f2720d3c3d7f597a1fd06d0fe48b8522b7b1b541
EzeSosa <ezequielsosa423@gmail.com>
2024-11-28 19:15:50 -0300

    feat: most sold products report endpoint

--- src/main/java/com/_up/megastore/services/interfaces/IReportService.java
+++ src/main/java/com/_up/megastore/services/interfaces/IReportService.java
@@ -1,11 +1,18 @@
 package com._up.megastore.services.interfaces;
 
 import com._up.megastore.data.reports.OrdersByStateReportResponse;
+import com._up.megastore.data.reports.SoldProductResponse;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface IReportService {
     OrdersByStateReportResponse getOrdersByStateAndDate(
+            LocalDate dateFrom,
+            LocalDate dateTo
+    );
+
+    List<SoldProductResponse> getMostSoldProductsByDate(
             LocalDate dateFrom,
             LocalDate dateTo
     );
