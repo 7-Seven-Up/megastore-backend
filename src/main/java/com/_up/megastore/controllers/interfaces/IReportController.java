add3cd9ff7b94f93ba4e1aaa900d6bd305259aa0
Federico Sanchez <fede14.sanchez@gmail.com>
2024-11-30 17:00:10 -0300

    [FTR] Add default parameters

--- src/main/java/com/_up/megastore/controllers/interfaces/IReportController.java
+++ src/main/java/com/_up/megastore/controllers/interfaces/IReportController.java
@@ -1,6 +1,7 @@
 package com._up.megastore.controllers.interfaces;
 
 import com._up.megastore.data.reports.OrdersByStateReportResponse;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,7 @@ public interface IReportController {
 
     @GetMapping("/orders-by-state")
     OrdersByStateReportResponse getOrdersByStateAndDate(
-            @RequestParam LocalDate dateFrom,
-            @RequestParam LocalDate dateTo
+            @RequestParam(required = false, defaultValue = "#{T(java.time.LocalDate).of(2024, 10, 1)}") @JsonFormat(pattern = "dd/MM/yyyy") LocalDate dateFrom,
+            @RequestParam(required = false, defaultValue = "#{T(java.time.LocalDate).now()}") @JsonFormat(pattern = "dd/MM/yyyy") LocalDate dateTo
     );
 }
